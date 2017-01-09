2cd288384fba77d2021a5aa238b8c80455f037e5
Mehulkumar Shah <rimpalshah@rimpals-MacBook-Pro.local>
2017-01-08 18:11:37 -0800

    second method

--- data-structures/removeDup.js
+++ data-structures/removeDup.js
@@ -2,9 +2,28 @@
  * Created by mshah on 1/8/17.
  */
 
+//Method 1
 var names = ["Mike","Matt","Nancy","Adam","Jenny","Nancy","Carl"];
 var uniqueNames = [];
 names.forEach(function(el,i){
     if(uniqueNames.indexOf(el) === -1) uniqueNames.push(el);
 });
-console.log(uniqueNames);
+// console.log(uniqueNames);
+
+
+// Method 2
+function withOutNewArr(a) {
+    var n = a.length;
+    var i, k;
+    k = 0;
+    for (i = 1; i < n; i++) {
+        if (a[k] != a[i]) {
+            a[k + 1] = a[i];
+            k++;
+        }
+    }
+    return a.slice(0, k + 1);
+}
+
+console.log(withOutNewArr([1, 1, 2, 2, 3, 3, 4, 4, 5, 5]));
+
